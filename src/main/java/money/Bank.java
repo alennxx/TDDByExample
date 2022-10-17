f87175637fd652c67dd6fed5e062e52643b55c4f
Natalia <natalia-gasiorowska@o2.pl>
2022-10-17 16:43:10 +0200

    removed dollar, franc; introduced bank and sum

---
+++ src/main/java/money/Bank.java
@@ -0,0 +1,11 @@
+package money;
+
+public class Bank {
+
+    public Money reduce(Expression source, String to) {
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
+    }
+
+}
--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,32 +1,46 @@
 package money;
 
-public abstract class Money {
+import java.util.Objects;
 
-    protected int amount;
-    protected final String currency;
+public class Money implements Expression {
+
+    final int amount;
+    final String currency;
 
     public Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
-    public abstract Money times(int multiplier);
-
     public String currency() {
         return currency;
     }
 
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    public Expression plus(Money addend) {
+        return new Sum(this, addend);
+    }
+
     @Override
     public boolean equals(Object object) {
-        return object != null && getClass().equals(object.getClass()) && ((Money) object).amount == amount;
+        return object instanceof Money && ((Money) object).amount == amount
+                && Objects.equals(((Money)object).currency, currency);
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
 }
