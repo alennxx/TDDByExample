a58b66f19512c41b731ecdddce0bd288d3ea5ab9
Natalia <natalia-gasiorowska@o2.pl>
2022-10-25 18:34:46 +0200

    ch23

--- TDDByExamplePython/TestCaseTest.py
+++ TDDByExamplePython/TestCaseTest.py
@@ -8,34 +8,33 @@ class TestCaseTest(TestCase):
 
     def __init__(self, name):
         super().__init__(name)
+    
+    def setUp(self):
+        self.result = TestResult()
 
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run(TestResult())
-        assert test.log == "setUp testMethod tearDown ", "Test was not correctly run!"
+        test.run(self.result)
+        assert "setUp testMethod tearDown " == test.log, "Test was not correctly run!"
     
     def testResult(self):
         test = WasRun("testMethod")
-        result = TestResult()
-        test.run(result)
-        assert "1 run, 0 failed" == result.summary(), "Result summary is not correct"
+        test.run(self.result)
+        assert "1 run, 0 failed" == self.result.summary(), "Result summary is not correct"
     
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = TestResult()
-        test.run(result)
-        assert "1 run, 1 failed", result.summary
+        test.run(self.result)
+        assert "1 run, 1 failed", self.result.summary
     
     def testFailedResultFormatting(self):
-        result = TestResult()
-        result.testStarted()
-        result.testFailed()
-        assert "1 run, 1 failed" == result.summary(), "Failed result summary is not correct"
+        self.result.testStarted()
+        self.result.testFailed()
+        assert "1 run, 1 failed" == self.result.summary(), "Failed result summary is not correct"
     
     def testSuite(self):
         suite = TestSuite()
         suite.add(WasRun("testMethod"))
         suite.add(WasRun("testBrokenMethod"))
-        result = TestResult()
-        suite.run(result)
-        assert "2 run, 1 failed" == result.summary(), "Test suite summary is not correct"
+        suite.run(self.result)
+        assert "2 run, 1 failed" == self.result.summary(), "Test suite summary is not correct"
